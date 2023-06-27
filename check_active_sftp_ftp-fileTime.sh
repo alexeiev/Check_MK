47498349c8423ed718e770b93cf1bb70a6c72178
Alexeiev Araujo <alexeiev@pop-os.localdomain>
2023-06-27 02:40:32 +0100

    bug fix: adjust function of getopts and function order

--- check_active_sftp_ftp-fileTime.sh
+++ check_active_sftp_ftp-fileTime.sh
@@ -8,55 +8,14 @@
 # alexeievfa@gmail.com      
 #
 # Example:
-# check_active_sftp_ftp-fileTime.sh -H 192.168.1.201 -u userservidor -d "~/data/" -D "ssh_files" -f .csv -t sftp -w 60
+# check_active_sftp_ftp-fileTime.sh -u userservidor -d "~/data/" -D "ssh_files" -f .csv -t sftp -w 60 -H 192.168.1.201 
 #
 
 #DEBUG=1 True
 #DEBUG=0 False
-DEBUG=0
+DEBUG=1
 
-ArqTemp=/tmp/TempFile_${Desc// /_}.txt
-NumFile=0
-Ind=0
-
-function Help() {
-    echo "Use: $0 <parm>
--h | --help This Help
--H <IP>
--d </directory/>
--D <Service_Description>
--u <userIP>
--f <file Extension>
--t <ftp (21) | sftp (22)>
--w <900> Time in sec.
-"
-exit
-}
-
-function T_DEPENDENCE() {
-Cmd=$@
-OP=0
-for Command in "$@";
-do
-    if [ ! $(which $Command 2>/dev/null) ];
-    then
-        OP=$(( $OP +1))
-        Problem[$OP]=$Command
-    fi
-done
-
-[ $DEBUG -eq 1 ] && echo "Commands problem - ${Problem[@]}"
-
-if [ ${#Problem[@]} -ne 0 ];
-then
-    echo "Command(s) not found - ${Problem[@]}"
-    exit 3
-fi
-}
-#Testing dependencies
-T_DEPENDENCE lftp
-
-# Add the following information separated by " : "
+# Add the following information separated by " | "
 #1 - userIpdaconexao
 #2 - Username
 #3 - Password
@@ -64,22 +23,53 @@ T_DEPENDENCE lftp
 # 'userHost|User|Password'
 dbPass=(
 'USERXsftp.server.com|USERX|##PASSWORD##'
-
+'user127|user|123'
 )
 
-while getopts :h:help:H:d:D:u:f:t:w: flag
+ArqTemp=/tmp/TempFile_${Desc// /_}.txt
+NumFile=0
+Ind=0
+
+function T_DEPENDENCE() {
+Cmd=$@
+OP=0
+for Command in "$@"
 do
-    case "${flag}" in
-        h) Help ;;
-        H) Host=${OPTARG};;
-        d) Dir=${OPTARG};;
-        D) Desc=${OPTARG};;
-        u) User=${OPTARG};;
-        f) File=${OPTARG};;
-        t) Command=${OPTARG};;
-        w) Warn=${OPTARG};;
-    esac
+    if [ ! $(which $Command 2>/dev/null) ]
+    then
+        OP=$(( $OP +1))
+        Problem[$OP]=$Command
+        if [ -z $OS ]
+        then
+            which apt >/dev/null && OS=debian
+            which yum >/dev/null && OS=redhat
+            [ $DEBUG -eq 1 ] && echo "OS - $OS"
+        fi
+    fi
 done
+
+if [ ${#Problem[@]} -ne 0 ]
+then
+    echo "Command(s) not found - ${Problem[@]}"
+    read -p "Do you want to install lftp?[Yn]" Yn
+    if [ -z $Yn ] || [ ${Yn^^} = "Y" ]
+    then
+        case $OS in
+            debian)
+                sudo apt install -y lftp >/dev/null 2>&1 && echo "successfully installed package"
+                exit 0
+            ;;
+            redhat)
+                sudo yum install -y lftp >/dev/null 2>&1 && echo "successfully installed package"
+                exit 0
+            ;;
+        esac
+    else
+        echo "This software ( ${Problem[@]} ) is prerequisite"
+        exit 1
+    fi
+fi
+}
 
 function Pass(){
     for key in ${dbPass[@]}
@@ -94,7 +84,38 @@ function Pass(){
     done
 }
 
+function Help() {
+    echo "Use: $(basename $0) [OPTIONS] <site>
+-h | --help This Help
+-d </directory/>
+-D <Service_Description>
+-u <userIP>
+-f <file Extension>
+-t <ftp (21) | sftp (22)>
+-w <900> Time in sec.
+-H <site>
+"
+exit
+}
 
+while getopts ':d:D:u:f:t:w:H:h' flag
+do
+    case "$flag" in
+        h) Help ;;
+        d) Dir=${OPTARG};;
+        D) Desc=${OPTARG};;
+        u) User=${OPTARG};;
+        f) File=${OPTARG};;
+        t) Command=${OPTARG};;
+        w) Warn=${OPTARG};;
+        H) Host=${OPTARG};;
+    esac
+done
+
+#Testing dependencies
+T_DEPENDENCE lftp
+
+#User and Pass
 Pass $User
 
 [ $DEBUG -eq 1 ] && echo "[DEBUG] - User: $User - Pass: $Pass"
